15535ba47952d42d197f31cdbf630b7abcf1c505
leinto <leinto0371@gmail.com>
2019-04-29 17:16:27 -0400

    product list and product details

---
+++ src/app/model/card.ts
@@ -0,0 +1,9 @@
+export interface Card {
+    verifiedTime: string,
+    manufacturer: string,
+    series: string,
+    model: string,
+    imgUrl: string,
+    year: number,
+    parameters: {}[]
+}
