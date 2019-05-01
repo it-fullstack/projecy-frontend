c31eb53f84e73e444ff358d19ccc89d8661772d6
hxiao3 <hxiao3@stevens.edu>
2019-05-01 06:14:07 -0400

    product compare

--- src/app/product-list/product-compare/product-compare.component.ts
+++ src/app/product-list/product-compare/product-compare.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../products.service';
+import * as $ from "jquery";
 
 @Component({
   selector: 'app-product-compare',
@@ -7,9 +9,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductCompareComponent implements OnInit {
 
-  constructor() { }
+  products = [];
+  Comparedps = [];
+  constructor(private productsService: ProductsService) { }
 
   ngOnInit() {
+    this.productsService.processData(this.productsService.getAllProductCard).subscribe(
+      () => {
+        this.products =  this.productsService.products;
+        for(let i = 0; i < this.products.length; i++){
+          let sum = $($.parseXML(this.products[i].summary));
+          //console.log(sum.find("manufacturer").text());
+          let VDate = new Date(parseInt(this.products[i].verifiedDate));
+          //console.log(VDate.toString());
+          this.Comparedps.push(new ComparedProduct(VDate.toString(),sum.find("manufacturer").text(),sum.find("series").text(),sum.find("model").text(),
+          sum.find("usetype").text(),sum.find("application").text(),sum.find("mountainLocation").text(),sum.find("year").text(),
+          sum.find("airflow").text(),sum.find("max-power").text(),sum.find("max-speed").text(),sum.find("fan-diameter").text(),sum.find("brand").text()));
+        }
+        console.log(this.Comparedps);
+        //console.log(this.products);
+      }
+    );
   }
 
+
+}
+
+export class ComparedProduct{
+  verifiedDate: string;
+  manufacturer: string;
+  series: string;
+  model: string;
+  usetype: string;
+  application: string;
+  mountainLocation: string;
+  year: string;
+  airflow: string;
+  max_power: string;
+  max_speed: string;
+  fan_diameter: string;
+  brand: string;
+  constructor(verifiedDate: string,manufacturer: string,series: string,model: string,usetype: string,application: string,mountainLocation: string,year: string, airflow: string,
+    max_power: string, max_speed: string, fan_diameter: string, brand: string) {
+      this.verifiedDate = verifiedDate;
+      this.manufacturer = manufacturer;
+      this.series = series;
+      this.model = model;
+      this.usetype = usetype;
+      this.application = application;
+      this.mountainLocation = mountainLocation;
+      this.year = year;
+      this.airflow = airflow;
+      this.max_power = max_power;
+      this.max_speed = max_speed;
+      this.fan_diameter = fan_diameter;
+      this.brand = brand;
+     }
 }
