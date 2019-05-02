e20195a137e1bdb8cdf011ef7fb9c386b3aea73c
leinto <leinto0371@gmail.com>
2019-05-02 14:08:56 -0400

    api

--- src/app/product-list/product-card/product-card.component.ts
+++ src/app/product-list/product-card/product-card.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductsService } from '../../_services/products.service';
-import { ActivatedRoute } from '@angular/router'
+import { Router, ActivatedRoute } from '@angular/router';
 
 
 @Component({
@@ -14,7 +14,7 @@ export class ProductCardComponent implements OnInit {
   subcategory: string;
   private sub: any;
 
-  constructor(private productsService: ProductsService, private activedRoute: ActivatedRoute) {
+  constructor(private productsService: ProductsService, private activedRoute: ActivatedRoute, private router: Router) {
     this.cards = this.productsService.productCard;
     this.productsService.observeProduct().subscribe(value => {
       this.cards = value;
@@ -24,7 +24,19 @@ export class ProductCardComponent implements OnInit {
   ngOnInit() {
     this.sub = this.activedRoute.params.subscribe(params => {
       this.subcategory = params["subcategory"];
-      this.productsService.processData(this.productsService.getProductCard, this.subcategory).subscribe(() => this.cards = this.productsService.productCard);
+      this.productsService.processData(this.productsService.getProductCard, this.subcategory).subscribe(
+        () => {
+          this.cards = this.productsService.productCard;
+          // console.log("kkk")
+          // console.log(this.cards);
+        });
     })
+  }
+  handleCompare() {
+    // this.cards.forEach(element => {
+    //   console.log(element.productId);
+    //   console.log(element.checked)
+    // });
+    // this.router.navigate(['productscompare']);
   }
 }
